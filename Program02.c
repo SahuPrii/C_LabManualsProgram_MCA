5b5e3bf666db8de414efdc1f76bf8385e0ac3da3
Priyanshu Sahu <87435429+SahuPrii@users.noreply.github.com>
2025-01-28 21:26:58 +0530

    Add files via upload

---
+++ Program02.c
@@ -0,0 +1,19 @@
+/* Pattern to solve this
+* * * * *
+* * * *
+* * *
+* * 
+*
+*/
+
+#include<stdio.h>
+int main(){
+    int i ,j;
+    for (i=5 ; i>=1; i--){   
+        for (j=1 ; j<=i ; j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
